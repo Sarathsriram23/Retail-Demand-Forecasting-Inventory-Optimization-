cca3c9f15337c24ba9d9e47597ee5bd000a654d9
Amenda Das <amendadasputhoor@gmail.com>
2026-07-22 20:45:14 +0530

    Add data loading and sales trend analysis

---
+++ Machine_Learning.py
@@ -0,0 +1,11 @@
+# generate a function to study sales trend over time using machine learning techniques
+import pandas as pd
+
+def load_data(\Users\amend\Downloads\Retail-Demand-Forecasting-Inventory-Optimization-\src\Data\calendar.csv):
+    """
+    Load the sales data from a CSV file.
+
+    """
+data = pd.read_csv(\Users\amend\Downloads\Retail-Demand-Forecasting-Inventory-Optimization-\src\Data\calendar.csv)
+        return data
+    
