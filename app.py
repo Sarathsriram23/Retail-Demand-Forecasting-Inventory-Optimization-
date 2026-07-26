799bf8cbf4962d800ed9215cc34b2b5c9b4f3bdd
radhikadhoot21 <radhikadhoot52@gmai.com>
2026-07-26 21:28:07 +0530

    Implemented data transformation fix, corrected melt logic, and completed functional Streamlit dashboard with charts and KPIs

--- app.py
+++ app.py
@@ -10,11 +10,27 @@ st.caption("Analyze sales trends and inventory performance")
 # Load real dataset (TEMPORARY)
 df = pd.read_csv("data/sales_train_validation.csv")
 
-# TEMP: create simple usable columns
+
 df = df.head(100)  # limit for performance
 
 df["date"] = range(len(df))
-df["sales"] = df.iloc[:, 6:].sum(axis=1)  # pick one sales column
+df["sales"] = df.iloc[:, 6:].sum(axis=1)
+
+
+# ✅ Select only day columns
+day_cols = [col for col in df.columns if col.startswith("d_")]
+
+df_long = df.melt(
+    id_vars=["id"],
+    value_vars=day_cols,   # 🔥 IMPORTANT FIX
+    var_name="day",
+    value_name="value"
+)
+
+
+df_long["day"] = df_long["day"].str.replace("d_", "").astype(int)
+
+df_long = df_long.head(1000) # pick one sales column
 
 # Sidebar
 st.sidebar.header("Filters")
@@ -32,7 +48,7 @@ col2.metric("Max Sales", int(df["sales"].max()))
 col3.metric("Min Sales", int(df["sales"].min()))
 # Chart
 st.subheader("Sales Trend")
-st.line_chart(df.set_index("date")["sales"])
+st.line_chart(df_long.groupby("day")["value"].sum())
 
 # Table
 st.subheader("Data")
@@ -44,4 +60,5 @@ st.subheader("Sales Distribution")
 st.bar_chart(filtered_df["sales"])
 
 st.subheader("Last 10 Days Sales")
-st.line_chart(filtered_df.tail(10).set_index("date")["sales"])
+last_10 = df_long.groupby("day")["value"].sum().tail(10)
+st.line_chart(last_10)
